3c9a92ab87927636515b170dee67ae36f9352cd3
Katrina H. Turner <khj@hawaii.edu>
2020-10-15 10:20:25 -1000

    options panel

--- src/module.ts
+++ src/module.ts
@@ -4,37 +4,19 @@ import { SimplePanel } from './SimplePanel';
 
 export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOptions(builder => {
   return builder
+    .addNumberInput({
+      path: 'numLines',
+      name: 'Number of Lines to Display',
+      defaultValue: 10,
+    })
     .addTextInput({
-      path: 'text',
-      name: 'Simple text option',
-      description: 'Description of panel option',
-      defaultValue: 'Default value of text input option',
+      path: 'leftHeader',
+      name: 'Left Column Header',
+      defaultValue: 'Left Title',
     })
-    .addBooleanSwitch({
-      path: 'showSeriesCount',
-      name: 'Show series counter',
-      defaultValue: false,
+    .addTextInput({
+      path: 'rightHeader',
+      name: 'Right Column Header',
+      defaultValue: 'Right Title',
     })
-    .addRadio({
-      path: 'seriesCountSize',
-      defaultValue: 'sm',
-      name: 'Series counter size',
-      settings: {
-        options: [
-          {
-            value: 'sm',
-            label: 'Small',
-          },
-          {
-            value: 'md',
-            label: 'Medium',
-          },
-          {
-            value: 'lg',
-            label: 'Large',
-          },
-        ],
-      },
-      showIf: config => config.showSeriesCount,
-    });
 });
