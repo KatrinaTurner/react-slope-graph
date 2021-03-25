aac68c3eff1392550f8827aea3f6326746d838cb
Katrina H. Turner <46706381+KatrinaTurner@users.noreply.github.com>
2021-03-25 11:59:27 -1000

    rename SimplePanel

---
+++ src/SlopeGraphPanel.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { PanelProps } from '@grafana/data';
+import { SlopeGraphOptions } from 'types';
+import { ParseData } from 'parser.js';
+import { Canvas } from 'components/Canvas';
+
+interface Props extends PanelProps<SlopeGraphOptions> {}
+
+export const SlopeGraphPanel: React.FC<Props> = ({ options, data, width, height, id }) => {
+  let graph_options = {
+    ...options
+  }
+
+  // new stuff
+//   let state = {
+//       data: [],
+//       activeName: null
+//   }
+  // ----
+
+  var parsedData = {};
+  try {
+    parsedData = ParseData(data, graph_options.numLines);
+
+    console.log(parsedData);
+    console.log(graph_options);
+  } catch (error) {
+      console.error("Parsing error : ", error);
+  }
+ 
+  return <Canvas height={height} width={width} panelId={id} options={graph_options} data={parsedData} />;
+
+};
