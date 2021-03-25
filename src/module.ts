7ee1f9caacc34c5aec9522308c2412fb92ae8d0b
Katrina H. Turner <46706381+KatrinaTurner@users.noreply.github.com>
2021-03-25 11:59:41 -1000

    update options

--- src/module.ts
+++ src/module.ts
@@ -1,42 +1,38 @@
-import { PanelPlugin } from '@grafana/data';
-import { SimpleOptions } from './types';
-import { SimplePanel } from './SimplePanel';
+import {
+  FieldConfigProperty,
+  PanelPlugin,
+} from '@grafana/data';
+import { SlopeGraphOptions } from './types';
+import { SlopeGraphPanel } from './SlopeGraphPanel';
+import { standardOptionsCompat } from 'grafana-plugin-support';
 
-export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOptions(builder => {
+const buildStandardOptions = (): any => {
+  const options = [FieldConfigProperty.Unit, FieldConfigProperty.Color];
+  return standardOptionsCompat(options);
+};
+
+export const plugin = new PanelPlugin<SlopeGraphOptions>(SlopeGraphPanel)
+.useFieldConfig({
+  useCustomConfig: (builder) => {
+    builder
+      .addSelect({
+        path: 'colorPalette',
+        name: 'Color palette',
+        // settings: {
+        //   options: buildColorPaletteOptions(),
+        // },
+        defaultValue: 'interpolateSpectral',
+      })
+      .addBooleanSwitch({
+        path: 'invertPalette',
+        name: 'Invert color palette',
+        defaultValue: false,
+      });
+    },
+  standardOptions: buildStandardOptions(),
+})
+.setPanelOptions(builder => {
   return builder
-    .addTextInput({
-      path: 'text',
-      name: 'Simple text option',
-      description: 'Description of panel option',
-      defaultValue: 'Default value of text input option',
-    })
-    .addBooleanSwitch({
-      path: 'showSeriesCount',
-      name: 'Show series counter',
-      defaultValue: false,
-    })
-    .addRadio({
-      path: 'seriesCountSize',
-      defaultValue: 'sm',
-      name: 'Series counter size',
-      settings: {
-        options: [
-          {
-            value: 'sm',
-            label: 'Small',
-          },
-          {
-            value: 'md',
-            label: 'Medium',
-          },
-          {
-            value: 'lg',
-            label: 'Large',
-          },
-        ],
-      },
-      showIf: config => config.showSeriesCount,
-    })
     .addNumberInput({
       path: 'numLines',
       name: 'Number of Lines to Display',
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 type SeriesSize = 'sm' | 'md' | 'lg';
 
-export interface SimpleOptions {
+export interface SlopeGraphOptions {
   text: string;
   showSeriesCount: boolean;
   seriesCountSize: SeriesSize;
